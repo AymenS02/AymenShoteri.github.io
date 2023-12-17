fb9bcfb11da6040ad090368850283b3e02caa165
Aymen Shoteri <aymenshoteri@gmail.com>
2023-12-17 17:26:21 -0500

    New Portfolio Website Design
    Beginning Production

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-  }
+document.addEventListener("DOMContentLoaded", function() {
+    const firstH3 = document.querySelector('nav-bar-h3-name');
+    
+    if(firstH3) {
+      firstH3.addEventListener('click', function() {
+        location.reload(); // Reloads the page when .first-h3 is clicked
+      });
+    }
+  });
